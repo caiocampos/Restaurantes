4eb4679f0f0c453f7f43bb81ee34de664acdce70
Caio Campos <caio.campos1204@gmail.com>
2021-08-09 20:15:54 -0300

    Correções para build

--- src/test/java/br/campos/restaurantes/BasicApplicationTests.java
+++ src/test/java/br/campos/restaurantes/BasicApplicationTests.java
@@ -1,12 +1,14 @@
 package br.campos.restaurantes;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
 public class BasicApplicationTests {
 
 	@Test
